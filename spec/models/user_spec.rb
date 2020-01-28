c47ba6ea810fcd866541586df8eaf9d5ae70f4b0
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-28 17:36:22 -0300

    TDD for users model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { User.new(email: 'example@email.com', password: '123456', name: 'User Example')}
+
+  before { subject.save }
+
+  it "should be valid" do
+    expect(subject).to be_valid
+  end
+
+  it "name should not be too long" do
+    subject.name = "a" * 26
+    expect(subject).to_not be_valid
+  end
+
+  it "name should not be too short" do
+    subject.name = "a"
+    expect(subject).to_not be_valid
+  end
+end
