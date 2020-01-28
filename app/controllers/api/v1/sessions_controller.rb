d2b5ad2d512e70b393e7ce1877ef86b0617ee739
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-28 18:17:34 -0300

    creating logged_in method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::SessionsController < ApplicationController
+  include CurrentUserConcern
   def create
     user = User
     .find_by(email: params[:session][:email])
@@ -12,6 +13,20 @@ class Api::V1::SessionsController < ApplicationController
       }
     else
       render json: { status: 401 }
+    end
+  end
+
+  def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      }
+    else {
+      render json: {
+        logged_in: false
+      }
+    }
     end
   end
 end
