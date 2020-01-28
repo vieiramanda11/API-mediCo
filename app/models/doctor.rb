7bff832665e5356084de2cb0d1b92e4c26d660ed
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-28 19:48:48 -0300

    adding association between tables

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,4 +3,6 @@ class Doctor < ApplicationRecord
   validates :speciality, presence: true, length: { in: 3..15 }
   validates :likes, presence: true, numericality: { only_integer: true }
   validates :years_of_experience, presence: true, numericality: { only_integer: true }
+  has_many :appointments
+  has_many :users, through: :appointments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true, length: { in: 3..24 }
+
+  has_many :appointments
+  has_many :doctors, through: :appointments
 end
