84268154bede0bec466a7154895cee0cd99f6c69
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-28 19:59:50 -0300

    creating filters to all parameters of doctors

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -2,7 +2,10 @@ class Api::V1::DoctorsController < ApplicationController
 
   DoctorReducer = Rack::Reducer.new(
     Doctor.all,
-    ->(name:) { where('lower(name) like ?', "%#{name.downcase}%") }
+    ->(name:) { where('lower(name) like ?', "%#{name.downcase}%") },
+    ->(speciality:) { where('lower(speciality) like ?', "%#{speciality.downcase}%") },
+    ->(likes:) { where(likes: likes) },
+    ->(years_of_experience:) { where(years_of_experience: years_of_experience) }
   )
   def index
     @doctors = DoctorReducer.apply(params)
