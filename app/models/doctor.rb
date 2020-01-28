3f9c0390b8e05afc61bc520fd09ec79521befadf
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-28 19:09:23 -0300

    adding validation to doctor's model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,6 @@
 class Doctor < ApplicationRecord
+  validates :name, presence: true, length: { in: 3..24 }
+  validates :speciality, presence: true, length: { in: 3..15 }
+  validates :likes, presence: true, numericality: { only_integer: true }
+  validates :years_of_experience, presence: true, numericality: { only_integer: true }
 end
