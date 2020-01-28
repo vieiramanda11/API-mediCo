94f0365a3ef9bf5e16c077528c1e7139fe1f3c79
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-28 19:53:32 -0300

    adding validations to appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,9 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :doctor
+
+  validates :user_id, presence: true
+  validates :doctor_id, presence: true
+  validates :date, presence: true
+  validates :time, presence: true
 end
