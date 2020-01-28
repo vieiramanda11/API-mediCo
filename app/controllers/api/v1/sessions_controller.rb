ac9ba9d292d0622b56b33bfe46e6b87f3d342953
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-28 18:18:34 -0300

    creating logout action

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -29,4 +29,12 @@ class Api::V1::SessionsController < ApplicationController
     }
     end
   end
+
+  def logout
+    reset_session
+    render json: {
+      status: 200,
+      logged_out: true
+    }
+  end
 end
